4f3c1bf90f68d293671a37c74452766a6b66a078
amank3125 <amank3125@gmail.com>
2022-08-31 19:40:37 +0530

    Update rto.js

--- rto.js
+++ rto.js
@@ -1,4 +1,3 @@
-console.log('Created by Aman')
 let waitTill = 0;
 let timer = document.querySelector('.timer');
 let currentLight = 'red';
@@ -13,6 +12,7 @@ let stopL = setInterval(stop,1000);
 let haltL;
 let goL;
 
+console.log('%c Created by Aman', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
 
 function stop(){
 
